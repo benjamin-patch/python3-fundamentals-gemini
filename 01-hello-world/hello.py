f7b3781f748098ea68fea9fe488abc65a1fdb38d
Benjamin Patch <ben@benjaminpatch.com>
2025-01-04 19:04:13 -0800

    Wrtie notes for 'Hello, World.'

--- 01-hello-world/hello.py
+++ 01-hello-world/hello.py
@@ -1,1 +1,7 @@
 print("Hello, World!")
+
+# Explanation
+
+# - print() is a built-in function in Python that displays output to the console.
+# - The text "Hello, World!" is a string literal, enclosed in double quotes.
+# - Single quotes can be used as well.
