8b911fe3b44c2a23dd3a4e82bf90f4ea192d38bd
Benjamin Patch <ben@benjaminpatch.com>
2025-01-04 19:21:53 -0800

    Remove bullet point indicators.

--- 01-hello-world/hello.py
+++ 01-hello-world/hello.py
@@ -2,6 +2,6 @@ print("Hello, World!")
 
 # Explanation
 
-# - print() is a built-in function in Python that displays output to the console.
-# - The text "Hello, World!" is a string literal, enclosed in double quotes.
-# - Single quotes can be used as well.
+# print() is a built-in function in Python that displays output to the console.
+# The text "Hello, World!" is a string literal, enclosed in double quotes.
+# Single quotes can be used as well.
